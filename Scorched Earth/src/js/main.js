2137d937d9a862e0642965271219d0669250d3e2
Engineer162 <164789696+Engineer162@users.noreply.github.com>
2025-01-16 09:15:54 +0100

    Add files via upload

--- Scorched Earth/src/js/main.js
+++ Scorched Earth/src/js/main.js
@@ -1,2 +1,17 @@
 import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
+import '/src/images/Sound_Enabled.png';
+import '/src/images/Sound_Disabled.png';
+
+// Attach event listener to the button
+document.getElementById('ToggleSoundButton').addEventListener('click', ToggleSound);
+
+function ToggleSound() {
+    const SoundState = document.getElementById('SoundState');
+    const SoundState1 = '/src/images/Sound_Enabled.png'; // Path to the first image
+    const SoundState2 = '/src/images/Sound_Disabled.png'; // Path to the second image
+  
+    // Switch image source
+    SoundState.src = SoundState.src.endsWith(SoundState1) ? SoundState2 : SoundState1;
+  }
+  
