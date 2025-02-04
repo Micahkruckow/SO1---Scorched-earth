f8a25fccef32bc064cb79edbe9a058df8a53acc9
Engineer162 <164789696+Engineer162@users.noreply.github.com>
2025-02-04 09:02:07 +0100

    Add files via upload

--- Scorched Earth/src/js/main.js
+++ Scorched Earth/src/js/main.js
@@ -5,11 +5,36 @@ import '/src/images/Sound_Disabled.png';
 import '/src/images/background.jpg';
 import '/src/images/Logo.svg';
 import '/src/pages/Game.txt';
+import '/src/pages/M109_pixelated';
 
 // Attach event listener to the Sound button, and the Play button.
 document.getElementById('ToggleSoundButton').addEventListener('click', ToggleSound);
 document.getElementById('PlayButton').addEventListener('click', LoadGame);
 object.onclick = function(){LoadCredits};
+
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+var canvas = document.getElementById('Game');
+var context = canvas.getContext('2d');
+var background = new Image();
+background.src = "/src/images/background.jpg";
+var tank = new Image();
+tank.src = "/src/pages/M109_pixelated";
+
+//Game state variables
+var GameIsLoaded = 0
+var GameIsRunning = 0
+
+//Background variables
+var Background_Xpos
+var Background_Xpos // Y position might not be used
+
+//Tank variables
+var tank_Xpos
+var tank_Ypos
+var GunIsLoading = 0
+var ShellsLeft = 36
 
 function ToggleSound() {
     const SoundState = document.getElementById('SoundState');
@@ -26,6 +51,7 @@ function LoadGame() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("Content").innerHTML =
        this.responseText;
+       GameIsLoaded = 1;
       }
     };
   xhttp.open("GET", "/src/pages/Game.txt", true);
@@ -42,4 +68,20 @@ function LoadCredits() {
   };
   xhttp.open("GET", "/src/pages/Credits.txt", true);
   xhttp.send();
+}
+
+if (GameIsLoaded == 1) {
+
+  while (GameIsRunning == 1) {
+    function Render(context) {
+
+    }
+
+    function animate() {
+      context.clearRect(0, 0, screenWidth, screenHeight);
+      Render(context);
+      setTimeout(animate, 5);
+    }
+    animate()
+  }
 }
